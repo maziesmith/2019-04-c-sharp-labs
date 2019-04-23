7b22c35bf7be2a200f41d6c4b38cb34f8986521a
philandersonsparta <panderson@spartaglobal.com>
2019-04-23 16:37:02 +0100

    Polymorphism And Abstract Classes

--- lab_40_abstract_class/Program.cs
+++ lab_40_abstract_class/Program.cs
@@ -10,6 +10,26 @@ namespace lab_40_abstract_class
     {
         static void Main(string[] args)
         {
+            // var h = new Holiday();
+            var h = new LetsGo(); // hey we're off!
+        }
+    }
+
+    abstract class Holiday
+    {
+        // REAL METHOD
+        void VisitGeysers() { 
+            // wow - have a great time! 
+        }
+        // ABSTRACT METHOD
+        public abstract void GettingToIceland();
+    }
+
+    class LetsGo : Holiday
+    {
+        public override void GettingToIceland()
+        {
+            // yes we do travel by plane and hire a car
         }
     }
 }
